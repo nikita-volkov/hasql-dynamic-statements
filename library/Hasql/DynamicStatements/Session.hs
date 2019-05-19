befd821e2cf88f7c75dd12225789ae83f731d06c
Nikita Volkov <nikita.y.volkov@mail.ru>
2019-05-19 17:34:35 +0300

    Implement Session

---
+++ library/Hasql/DynamicStatements/Session.hs
@@ -0,0 +1,14 @@
+module Hasql.DynamicStatements.Session where
+
+import Hasql.DynamicStatements.Prelude
+import Hasql.Session
+import qualified Hasql.DynamicStatements.Snippet.Defs as SnippetDefs
+import qualified Hasql.DynamicStatements.Statement as Statement
+import qualified Hasql.Decoders as Decoders
+import qualified Hasql.Session as Session
+
+{-|
+Execute a dynamically constructed statement, providing a result decoder.
+-}
+snippet :: SnippetDefs.Snippet -> Decoders.Result result -> Session result
+snippet snippet decoder = Session.statement () (Statement.snippet snippet decoder)
