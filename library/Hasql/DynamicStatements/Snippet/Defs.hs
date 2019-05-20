dd3d2b89e011c4bfccbd99a38e662ed80aa0b4f3
Nikita Volkov <nikita.y.volkov@mail.ru>
2019-05-20 13:05:12 +0300

    Add docs

--- library/Hasql/DynamicStatements/Snippet/Defs.hs
+++ library/Hasql/DynamicStatements/Snippet/Defs.hs
@@ -5,6 +5,9 @@ import qualified Hasql.Encoders as Encoders
 import qualified Hasql.DynamicStatements.ImplicitEncoders as ImplicitEncoders
 
 
+{-|
+Dynamically generated statement.
+-}
 newtype Snippet = Snippet (Seq SnippetChunk)
 
 data SnippetChunk =
@@ -17,11 +20,20 @@ deriving instance Monoid Snippet
 instance IsString Snippet where
   fromString x = Snippet (pure (TextSnippetChunk (fromString x)))
 
+{-|
+SQL chunk in ASCII.
+-}
 sql :: ByteString -> Snippet
 sql x = Snippet (pure (TextSnippetChunk x))
 
+{-|
+Parameter encoded using an implicitly derived encoder from the type.
+-}
 param :: ImplicitEncoders.ParamsEncoder param => param -> Snippet
 param param = Snippet (pure (ParamSnippetChunk (param >$ ImplicitEncoders.params)))
 
+{-|
+Parameter with an explicitly defined encoder.
+-}
 encoderAndParam :: Encoders.Params param -> param -> Snippet
 encoderAndParam encoder param = Snippet (pure (ParamSnippetChunk (param >$ encoder)))
