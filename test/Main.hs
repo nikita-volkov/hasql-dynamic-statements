2abb6d4d2cd823fa1889529d1b3ca21af2b4beef
Wolfgang Walther <walther@technowledgy.de>
2024-01-21 21:08:32 +0100

    Modernize CI

--- test/Main.hs
+++ test/Main.hs
@@ -70,7 +70,7 @@ withConnection handler =
             host = "localhost"
             port = 5432
             user = "postgres"
-            password = ""
+            password = "postgres"
             database = "postgres"
     use connection =
       lift $ handler connection
